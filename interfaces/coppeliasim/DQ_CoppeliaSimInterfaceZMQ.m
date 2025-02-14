de30e9471748a394ace23b3c121ab8bda59b9f09
Juan <juanjqogm@gmail.com>
2025-02-14 16:07:01 +0000

    [DQ_CoppeliaSimInterfaceZMQ] Improved the documentation

--- interfaces/coppeliasim/DQ_CoppeliaSimInterfaceZMQ.m
+++ interfaces/coppeliasim/DQ_CoppeliaSimInterfaceZMQ.m
@@ -47,7 +47,16 @@ classdef DQ_CoppeliaSimInterfaceZMQ < DQ_CoppeliaSimInterface
         end
 
         function rtn = connect_(obj, host, rpcPort, MAX_TIME_IN_MILLISECONDS_TO_TRY_CONNECTION, cntPort, verbose)
-
+                % This method initializes the remote API client. 
+                %        host: The IP address of the computer that hosts the CoppeliaSim simulation. If the client (your code)
+                %             and the simulation are running in the same computer, you can use "localhost".
+                %        port: The port to establish a connection. (e.g. 23000, 23001, 23002, 23003...).
+                %        TIMEOUT_IN_MILISECONDS The timeout to establish the connection. 
+                %                               However, this timeout feature is not implemented yet.
+                %        cntPort: This parameter is not well documented on
+                %        the remote API. A typical value is -1
+                %        verbose: This parameter is not well documented on
+                %        the remote API. A typical value is false.
                 obj.host_ = host;
                 obj.rpcPort_ = rpcPort;
                 obj.cntPort_ = cntPort;
@@ -65,17 +74,26 @@ classdef DQ_CoppeliaSimInterfaceZMQ < DQ_CoppeliaSimInterface
                 catch ME
                     rethrow(ME)
                 end
-
                 rtn = true;
         end
 
         function check_client_(obj)
+            % This method throws an exception if the client is not
+            % initialized. 
             if (~obj.client_created_)
                 error('Unestablished connection. Did you use connect()?');
             end
         end
 
         function set_status_bar_message_(obj, message, verbosity_type)
+            % This method sets a message on the status bar of CoppeliaSim
+            % Usage:
+            %     set_status_bar_message_('DQ Robotics', obj.sim_.verbosity_warnings)
+            %     
+            %     message. The desired message to show on the status bar.
+            %     verbosity_type. The verbosity level, as explained in the
+            %                     documentation:
+            %                     https://manual.coppeliarobotics.com/en/apiConstants.htm#verbosity
             obj.sim_.addLog(verbosity_type, message);
         end
 
@@ -87,20 +105,24 @@ classdef DQ_CoppeliaSimInterfaceZMQ < DQ_CoppeliaSimInterface
             obj.client_created_ = false;
         end
         
-        function connect(obj, host, port, TIMEOUT_IN_MILISECONDS)
-            % This method connects to the remote api server (i.e. CoppeliaSim).
+        function status = connect(obj, host, port, TIMEOUT_IN_MILISECONDS)
+            % This method connects to the remote api server (i.e.CoppeliaSim). 
+            % Returns true if the connection is established. False otherwise.
             % Calling this function is required before anything else can happen.
+            %
+            % Usage: vi.connect(host, port, TIMEOUT_IN_MILISECONDS)
+            %
+            %        host: The IP address of the computer that hosts the CoppeliaSim simulation. If the client (your code)
+            %             and the simulation are running in the same computer, you can use "localhost".
+            %        port: The port to establish a connection. (e.g. 23000, 23001, 23002, 23003...).
+            %        TIMEOUT_IN_MILISECONDS The timeout to establish the connection.       
             arguments
                 obj  (1,1) DQ_CoppeliaSimInterfaceZMQ
                 host (1,1) {mustBeText} = 'localhost'
                 port (1,1) {mustBeNumeric} = 23000
                 TIMEOUT_IN_MILISECONDS (1,1) {mustBeNumeric} = 1000
-            end
-
-            
-           obj.connect_(host, port, TIMEOUT_IN_MILISECONDS, -1, false);
-
-
+            end     
+            status = obj.connect_(host, port, TIMEOUT_IN_MILISECONDS, -1, false);
         end
 
         function set_stepping_mode(obj, flag)
